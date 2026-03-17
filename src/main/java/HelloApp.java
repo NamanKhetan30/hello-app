75a95cd61646d01e845d9228b5175edb42bf27a0
NamanKhetan30 <khetannaman2006@gmail.com>
2026-03-17 13:15:49 +0530

    UC2: Display Hello with command line argument

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,12 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        if (args.length == 0) {
+            System.out.println("Please provide your name.");
+            return;
+        }
+
+        String name = args[0];
+        System.out.println("Hello " + name);
     }
 }
