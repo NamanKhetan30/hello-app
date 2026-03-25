556dd556f7ffb494a4bb0dee7c5dbc2ae32119c8
NamanKhetan30 <khetannaman2006@gmail.com>
2026-03-25 22:52:28 +0530

    UC3: Default name as World

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,12 +1,8 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        if (args.length == 0) {
-            System.out.println("Please provide your name.");
-            return;
-        }
+        String name = (args.length == 0) ? "World" : args[0];
 
-        String name = args[0];
         System.out.println("Hello " + name);
     }
 }
