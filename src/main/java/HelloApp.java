0f37607f70f035533ff4b27b5144e4769b2b811c
NamanKhetan30 <khetannaman2006@gmail.com>
2026-03-25 22:55:36 +0530

    UC4: Display Hello for multiple names

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -2,11 +2,11 @@ public class HelloApp {
     public static void main(String[] args) {
 
         if (args.length == 0) {
-            System.out.println("Please provide your name.");
-            return;
+            System.out.println("Hello World");
+        } else {
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
         }
-
-        String name = args[0];
-        System.out.println("Hello " + name);
     }
 }
